0c95085bdb12ac0225fa339b10e06102d2b16217
isabelrios <isabelrios@gmail.com>
2021-03-22 15:14:11 +0100

    add changes in bash script

--- read_tag.sh
+++ read_tag.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 filename="current_tag.txt"
-echo $filename
-echo "read"
 #!/usr/bin/bas
 while read -r line; do
     name="$line"
-    echo "Name read from file - $name"
 done < "$filename"
 echo $name
