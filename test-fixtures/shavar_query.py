29c29a7629f7a9da6fda68bdb04098d97cf991f1
isabelrios <isabelrios@gmail.com>
2021-05-11 18:13:49 +0200

    add comments to explain script

--- test-fixtures/shavar_query.py
+++ test-fixtures/shavar_query.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 import requests
 import shutil
 import tempfile
@@ -9,12 +10,23 @@ ORIGINAL_SHAVAR_FILE = PATH_TO_FILES + '/base-fingerprinting-track.json'
 TMP_SHAVAR_FILE = './content-blocker/tmp.json'
 tmp = tempfile.NamedTemporaryFile()
 
+
+'''
+STEPS
+1. POST to shavar url
+2. Process the response to get the u value
+3. Append .json to u value to compose the new url: name_of_the_list.json
+4. GET to the new url to retrieve the list
+5. Compare the json list with the local json list
+'''
+
+
 class Shavar:
 
     def __init__(self):
         pass
-    
-    def query_download_file(self):
+
+    def query_shavar_file_with_u_value(self):
         url = "https://shavar.stage.mozaws.net/downloads?appver=86.0&pver=2.2"
         payload = "base-fingerprinting-track-digest256;"
 
@@ -26,12 +38,10 @@ class Shavar:
     def process_data_to_get_new_url(self, u_value):
         with open(tmp.name, 'w') as f:
             f.write(u_value)
-     
+
         with open(tmp.name) as file:
-            rows = ( line.split(':') for line in file)
-            dict = { row[0]:row[1] for row in rows }
-        for item in dict:
-            print(dict[item])
+            rows = (line.split(':') for line in file)
+            dict = {row[0]: row[1] for row in rows}
 
         append_str = '.json'
         new_string = dict["u"].strip("\n")
@@ -39,11 +49,11 @@ class Shavar:
         print(new_url)
         return new_url
 
-    def get_list(self, new_url):
+    def get_list_json_format(self, new_url):
         res = requests.get(new_url)
         new_list = res.json()
         new_list_str = str(new_list["domains"])
-        new_list_quotes = new_list_str.replace("'",'"').replace(",", ",\n").replace("[", "[\n ").replace("]", "\n]")
+        new_list_quotes = new_list_str.replace("'", '"').replace(",", ",\n").replace("[", "[\n ").replace("]", "\n]")
         print(new_list_quotes)
 
         with open("./content-blocker/tmp.json", 'w') as f:
@@ -56,22 +66,22 @@ class Shavar:
 
         file_1 = json.dumps(json1, sort_keys=True)
         file_2 = json.dumps(json2, sort_keys=True)
-        
-        if (file_1 != file_2): 
-            shutil.move('./content-blocker/tmp.json', './content-blocker/base-fingerprinting-track.json')
+
+        if (file_1 != file_2):
+            shutil.move('PATH_TO_FILES/tmp.json', 'PATH_TO_FILES/base-fingerprinting-track.json')
             print("content blocker file changed")
         else:
             os.remove('./content-blocker/tmp.json')
-            print("No changes, no need to commit and remove temporary file created")
+            print("No changes in shavar list")
 
 
 def main():
     s = Shavar()
-    u_value = s.query_download_file()
+    u_value = s.query_shavar_file_with_u_value()
     new_url = s.process_data_to_get_new_url(u_value)
-    s.get_list(new_url)
+    s.get_list_json_format(new_url)
     s.compare_file(ORIGINAL_SHAVAR_FILE, TMP_SHAVAR_FILE)
+
 
 if __name__ == '__main__':
     main()
-
