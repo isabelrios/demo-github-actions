0be6d0d00450921842a2062f932639aacf7c1f82
isabelrios <isabelrios@gmail.com>
2022-12-15 12:48:55 +0100

    add workflow to auto update content-blocker file

---
+++ content-blocker.sh
@@ -0,0 +1,8 @@
+# Read version txt
+CURRENT_SHAVAR_LIST_VERSION="0"
+
+# Download shavar prod list
+echo "shavar-lists-v${CURRENT_SHAVAR_LIST_VERSION}"
+mkdir shavar-lists-v${CURRENT_SHAVAR_LIST_VERSION}
+
+# Download all the lists from the correct folder
